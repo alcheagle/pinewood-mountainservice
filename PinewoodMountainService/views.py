8497b6b04dd8e691d9f5fbd746e1c36fb62d5af0
Alessandro Cacco <alessandro.cacco@studenti.unitn.it>
2016-09-13 21:38:53 +0200

    Gil's magic wand happened to pass here

--- PinewoodMountainService/views.py
+++ PinewoodMountainService/views.py
@@ -3,29 +3,28 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tracking index.")
+    return HttpResponse("Hello, world. You're at the main index.")
 
-from tracking.forms import UploadForm
+from PinewoodMountainService.forms import MainForm
 from django.views.generic.edit import FormView
 
-from file_parser import file_parser
-
-class UploadView(FormView):
-    template_name = 'tracking/form.html'
-    form_class = UploadForm
+class MainPageView(FormView):
+    template_name = 'main/form.html'
+    form_class = MainForm
     success_url = '/thanks/'
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
-        return super(UploadView, self).form_valid(form)
+        form.send_email()
+        return super(MainPageView, self).form_valid(form)
 
 def upload_file(request):
     if request.method == 'POST':
-        form = UploadForm(request.POST, request.FILES)
+        form = MainForm(request.POST, request.FILES)
         if form.is_valid():
             file_parser(request.FILES['file'])
             return HttpResponseRedirect('/success/url/')
     else:
-        form = UploadForm()
+        form = MainForm()
     return render(request, 'upload.html', {'form': form})
--- tracking/urls.py
+++ tracking/urls.py
@@ -4,7 +4,7 @@ from . import views
 from . import json_view
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
+
     url(r'^$', views.UploadView.as_view()),	
     url(r'^track-(?P<track_id>)$/', json_view.print_json),	
 ]
