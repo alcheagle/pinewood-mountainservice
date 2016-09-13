589943c29bbdab2a9d699893a0638a60340c63a1
Andrea Gilardoni <andrea.gilardoni96@gmail.com>
2016-09-13 18:26:38 +0200

    fixing bad commit

--- tracking/urls.py
+++ tracking/urls.py
@@ -3,11 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    url(r'^$', views.index, name='index'),
-]
-=======
     #url(r'^$', views.index, name='index'),
     url(r'^$', views.UploadView.as_view()),	
 ]
->>>>>>> dd497379172b25363fcda92466d4be67315686e9
