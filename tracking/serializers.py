50424a3e4ff599f369c50e604ec200c9eb90684d
Andrea Gilardoni <andrea.gilardoni96@gmail.com>
2016-09-13 18:21:22 +0200

    Created serializers for data

---
+++ tracking/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from tracking.models import *
+
+class PositionSerializer(serializers.Serializer):    
+
+    def create(self, validated_data):
+        return Position.objects.create(**validated_data)
+
+class SegmentSerializer(serializers.Serializer):
+    begin = PositionSerializer(many=False, read_only=True)
+    end = PositionSerializer(many=False, read_only=True)
+
+    def create(self, validated_data):
+        return Segment.objects.create(**validated_data)
+
+class TrackSerializer(serializers.Serializer):
+    tracks = SegmentSerializer(many=True, read_only=True)
+
+    def create(self, validated_data):
+        return Track.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance._id = validated_data.get('_id', instance._id)
+        instance.descr = validated_data.get('descr', instance.descr)
+        instance.tracks = validated_data.get('tracks', instance.tracks)
+        instance.save()
+
+        return instance
+        
