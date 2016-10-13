e37f7d119ad4774321cde2e07b339a06189d8575
Enrico Saccon <Enrico@MacBook-Pro.local>
2016-10-13 09:46:28 +0200

    Boh

--- PinewoodMountainService/settings.py
+++ PinewoodMountainService/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'PinewoodMountainService.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'PinewoodMountainService/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'PinewoodMountainService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        #'ENGINE': 'mysql.connector.django', 
+        'ENGINE': 'django.db.backends.mysql',
+        #'ENGINE': 'mysql.connector.django',
         'NAME': 'pinewood',
         'USER': 'pinewood',
         'PASSWORD': 'pinewood',
