ab1707689954e2004dcc82ffc5899c360907b08d
Andrea Gilardoni <andrea.gilardoni96@gmail.com>
2016-10-25 17:36:43 +0200

    Added 404 Message
    Added 404 Message while receiving the wrong payload in autodeploy service

--- autodeploy/views.py
+++ autodeploy/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
-import json
-import os
-import sys
+import json, os, sys
 
 PATH                    = '/var/www/pinewood-mountainservice'
 CONFIG_FILE_NAME        = '.config.json'
@@ -37,21 +35,25 @@ def hook_handler(request):
         github_post = json.loads(request.body.decode("utf-8")) #FIXME there could be no json object
 
         if "ref" in github_post:
+            GIT_COMMAND = ["git", "--git-dir={}/.git".format(os.path.join(PATH)), "--work-tree={}".format(PATH)]
+            MANAGE_COMMAND = ['{}/manage.py'.format(PATH)]
+
             pushed_branch = re.search("^.{1,}\/.{1,}\/(.{1,})$", github_post['ref']).group(1) #TODO use compiled regex instead
             print(pushed_branch)
             from subprocess import call, check_output, Popen
 
-            out = check_output(["git", "--git-dir={}/.git".format(os.path.join(PATH)), "--work-tree={}".format(PATH),  "branch"]) #FIXME find why the server has troubles while running git commands, it seems to be in the right directory and as the right user
+            out = check_output(GIT_COMMAD + ["branch"]) #FIXME find why the server has troubles while running git commands, it seems to be in the right directory and as the right user
             actual_branch = re.search("^\* (.{1,})$", out.decode("utf-8")).group(1)
 
             if actual_branch == pushed_branch:
-                res1 = check_output(["git", "--git-dir={}".format(os.path.join("{}/.git".format(PATH))), "--work-tree={}".format(PATH), "pull", "origin", actual_branch])
+                #check if the server is already up-to-date
+                res1 = check_output(GIT_COMMAND + ["pull", "origin", actual_branch])
 
-                res2 = check_output(['{}/manage.py'.format(PATH), 'makemigrations'])
-                res3 = check_output(['{}/manage.py'.format(PATH), 'migrate'])
+                res2 = check_output(MANAGE_COMMAND + ['makemigrations'])
+                res3 = check_output(MANAGE_COMMAND + ['migrate'])
 
                 res4 = call(['sudo', 'service', 'apache2.service', 'reload']) #WARNING this could be harmful, verify that is really github posting
 
                 return HttpResponse("{} </br> {} </br> {} </br> {}".format(res1, res2, res3, res4))
             return HttpResponse("Nothing to do here")
-    return HttpResponse('this link is used to update the git repo')
+    raise Http404
